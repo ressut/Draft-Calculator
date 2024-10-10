287121e6bc51d0bcb0873c4125f4cf08cd0fdd66
ressut <47404112+ressut@users.noreply.github.com>
2024-10-10 12:07:50 +0100

    Add files via upload

---
+++ draft calculator.py
@@ -0,0 +1,22 @@
+#Input three values - Calculate draft of vessel – Output draft and echo inputs
+
+#Input
+length = float(input("Enter Length value:"))
+breadth = float(input("Enter Breadth value:"))
+height = float(input("Enter Height value:"))
+
+#Calculation
+iron_weight = 1.06 #constant
+area_1 = 2*(length*height)
+area_2 = 2*(breadth*height)
+walls_area = area_1+area_2
+floor_area = length*breadth
+surface_area = walls_area+floor_area
+mass = surface_area*iron_weight
+draft = mass/(length*breadth)
+
+#Output
+print(f"Length: {length} Breadth: {breadth} Height: {height}")
+print(f"Surface Area: {surface_area}")
+print(f"Mass: {mass}")
+print(f"Draft: {draft:.2f}")
